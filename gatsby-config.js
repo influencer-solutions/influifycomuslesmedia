b827dfe856f4fda209ea075a80452975eafab04a
AydenMusles <41048702+AydenMusles@users.noreply.github.com>
2020-01-18 00:47:05 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,44 @@ module.exports = {
         short_name: 'Gatbsy + Node.js (TypeScript)',
         start_url: '/',
         icon: 'src/images/gatsby-icon.png',
+        /*
+   * Gatsby's data processing layer begins with “source”
+   * plugins. Here the site sources its data from Shopify.
+   */
+  {
+    resolve: "gatsby-source-shopify",
+    options: {
+      // The domain name of your Shopify shop. This is required.
+      // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
+      // 'gatsby-source-shopify-test-shop.myshopify.com'.
+      // If you are running your shop on a custom domain, you need to use that
+      // as the shop name, without a trailing slash, for example:
+      // shopName: "gatsby-shop.com",
+      shopName: "gatsby-source-shopify-test-shop",
+
+      // An API access token to your Shopify shop. This is required.
+      // You can generate an access token in the "Manage private apps" section
+      // of your shop's Apps settings. In the Storefront API section, be sure
+      // to select "Allow this app to access your storefront data using the
+      // Storefront API".
+      // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
+      accessToken: "example-wou7evoh0eexuf6chooz2jai2qui9pae4tieph1sei4deiboj",
+
+      // Set verbose to true to display a verbose output on `npm run develop`
+      // or `npm run build`. This prints which nodes are being fetched and how
+      // much time was required to fetch and process the data.
+      // Defaults to true.
+      verbose: true,
+
+      // Number of records to fetch on each request when building the cache
+      // at startup. If your application encounters timeout errors during
+      // startup, try decreasing this number.
+      paginationSize: 250,
+
+      // List of collections you want to fetch.
+      // Possible values are: 'shop' and 'content'.
+      // Defaults to ['shop', 'content'].
+      includeCollections: ["shop", "content"],
       },
     },
   ],
