bb4327be28419c37e8c62f4cc688db75f5a1a0e4
AydenMusles <41048702+AydenMusles@users.noreply.github.com>
2020-01-18 00:49:09 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,28 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+
+  const pages = await graphql(`
+    {
+      allShopifyProduct {
+        edges {
+          node {
+            id
+            handle
+          }
+        }
+      }
+    }
+  `)
+
+  pages.data.allShopifyProduct.edges.forEach(edge => {
+    createPage({
+      path: `/${edge.node.handle}`,
+      component: path.resolve("./src/templates/product.js"),
+      context: {
+        id: edge.node.id,
+      },
+    })
+  })
+}
